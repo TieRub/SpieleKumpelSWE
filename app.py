289c6af905d37372c292a7d5b7fb2c590ace4e8a
Marcel.Grauerholz <marcel.grauerholz@stud.th-luebeck.de>
2024-12-25 15:22:59 +0100

    Versuch Datenbank zu erstellen. Das hinzufügen zu den Dateien über die Konsole funktioniert noch nicht. app context fehlt??

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class User(db.Model):
+    username = db.Column(db.String(20), primary_key=True)
 
 @app.route('/')
 def hello_world():  # put application's code here
