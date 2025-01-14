4c45b5c2532cc56921ffa86365ea9c41323ed3bb
TieRub <steven.niclas.baumgart@gmail.com>
2025-01-14 18:27:33 +0100

    Version 0.02

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
+from flask import Flask, session, render_template, redirect, url_for, request
+app = Flask(__name__, template_folder='src/html', static_folder='src')
+@app.route('/index')
+def index():
+    return render_template('pages/index.html')
+
+@app.route('/base')
+def base():
+    return render_template('pages/base.html')
+
+@app.route('/logging')
+def logging():
+    return render_template('pages/logging.html')
--- filestructure.md
+++ filestructure.md
@@ -1,9 +1,17 @@
 |--     /src
 |--     |-- /database
+|--     |   |-- users.sql
 |--     |-- /html
 |--     |   |-- /assets
 |--     |   |-- /pages
+|--     |   |   |-- base.html
+|--     |   |   |-- index.html
+|--     |   |   |-- logging.html
 |--     |   |-- /scripts
+|--     |   |   |-- script-logging.js
 |--     |   |-- /styles
+|--     |   |   |-- style-logging.css
 |--     |-- /services
+|--     |   |-- logging.py
 |--     app.py
+|--     Spielekumpel-DB.sqlite
