055a5b868d7faf322b02848be09d823a7c46e76d
TieRub <steven.niclas.baumgart@gmail.com>
2025-01-14 22:40:54 +0100

    Version 0.2.5 (added site dummys)

--- app.py
+++ app.py
@@ -7,12 +7,37 @@ def create_app():
     app = Flask(__name__, template_folder='src/html/pages', static_folder='src/html')
     app.secret_key = 'your_secret_key'
 
-    # index.html
+# index.html
     @app.route('/')
     def index():
         return render_template('index.html')
 
-    # login.html
+#event
+    @app.route('/events_create')
+    def events_create():
+        return render_template('event/events_create.html')
+    @app.route('/events_edit')
+    def events_edit():
+        return render_template('event/events_edit.html')
+    @app.route('/events_view')
+    def events_view():
+        return render_template('event/events_view.html')
+
+#user
+    @app.route('/friends_search')
+    def friends_search():
+        return render_template('user/friends_search.html')
+    @app.route('/friends_view')
+    def friends_view():
+        return render_template('user/friends_view.html')
+    @app.route('/profile_edit')
+    def profile_edit():
+        return render_template('user/profile_edit.html')
+    @app.route('/profile_view')
+    def profile_view():
+        return render_template('user/profile_view.html')
+
+#logging
     @app.route('/login', methods=['GET', 'POST'])
     def login():
         if request.method == 'POST':
@@ -24,8 +49,6 @@ def create_app():
                 flash('Invalid username or password', 'error')
                 return redirect(url_for('login'))
         return render_template('logging/login.html')
-
-    # register.html
     @app.route('/register', methods=['GET', 'POST'])
     def register():
         if request.method == 'POST':
@@ -38,8 +61,6 @@ def create_app():
                 flash('Username already exists, please choose a different one', 'error')
                 return redirect(url_for('register'))
         return render_template('logging/register.html')
-
-    # Logout route
     @app.route('/logout')
     def logout_route():
         return logout()
