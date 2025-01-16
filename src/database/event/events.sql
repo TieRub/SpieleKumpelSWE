d936d322c081fbd7cb14c26d3d767ac293a01687
TieRub <steven.niclas.baumgart@gmail.com>
2025-01-16 03:14:22 +0100

    Version 0.3.9.1 (updated tables events)

---
+++ src/database/event/events.sql
@@ -0,0 +1,14 @@
+CREATE TABLE events (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    creator_id INTEGER NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    activity VARCHAR(100) NOT NULL,
+    max_participants INTEGER NOT NULL,
+    is_public BOOLEAN NOT NULL DEFAULT 1,
+    description TEXT,
+    event_date TIMESTAMP NOT NULL,
+    current_participants INTEGER NOT NULL DEFAULT 0,
+    FOREIGN KEY (creator_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+DROP TABLE events;
